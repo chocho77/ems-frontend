d42884c7adeb336baa7f3686d7e0b28c99e0bb4b
Chavdar Momchilov <chavmom@gmail>
2024-11-25 13:45:20 +0200

    On branch master  Initial commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   eslint.config.js
    	new file:   index.html
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/vite.svg
    	new file:   src/App.css
    	new file:   src/App.jsx
    	new file:   src/HelloWorld.jsx
    	new file:   src/assets/react.svg
    	new file:   src/components/EmployeeComponent.jsx
    	new file:   src/components/FooterComponent.jsx
    	new file:   src/components/HeaderComponent.jsx
    	new file:   src/components/ListEmployeeComponent.jsx
    	new file:   src/index.css
    	new file:   src/main.jsx
    	new file:   src/services/EmployeeService.js
    	new file:   vite.config.js

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved 2013 by javaguides</span>
+
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
